75df28fbddf9f59ab31dbbcbfa8a89f07010caed
DAVID <jd0400887@gmail.com>
2025-11-14 15:15:03 -0500

    feat: Add PDF report generation

--- vite.config.ts
+++ vite.config.ts
@@ -39,4 +39,12 @@ export default defineConfig({
       }
     })
   ],
+  server: {
+    proxy: {
+      '/.netlify/functions': {
+        target: 'http://localhost:8888',
+        changeOrigin: true,
+      },
+    },
+  },
 })
